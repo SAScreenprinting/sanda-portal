9397dba5ddd8f644e5dbef752f165a6b981191f2
SAScreenprinting <sascreenprinting@outlook.com>
2026-05-10 22:43:52 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.ssactivewear.com',
+        pathname: '/Images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
